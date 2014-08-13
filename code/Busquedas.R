d3a03662e168c6f46c9f2f189032811e33960c1d
newtsukisan <trabajo@mbp-jal-3.home>
2014-08-13 18:54:40 +0200

    Incluye en la parte de busquedas una para Salamanca

--- code/Busquedas.R
+++ code/Busquedas.R
@@ -1,4 +1,8 @@
 
 source("funciones/funciones.R")
-
+# Busqueda en nueva España
 # proccessDataFromIdealista("resultados","nuevaEsp",40.462926, -3.670845,555,pag=TRUE,deb=TRUE)
+
+# Busqueda en Salamanca 1
+proccessDataFromIdealista("resultados","Salamanca",40.426195, -3.674118, 480, pag=TRUE, deb=TRUE)
+
