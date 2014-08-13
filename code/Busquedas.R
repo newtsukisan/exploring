ed1b9b4dd854c62ca55189e8c289f88c53de4f42
Jose A <newtsukisan@gmail.com>
2014-08-13 18:58:27 +0200

    Version funcional

--- code/Busquedas.R
+++ code/Busquedas.R
@@ -1,4 +1,5 @@
-
+# 0. El objetivo de este fichero es realizar todo el proceso de descarga de datos desde el API de
+# idealista, la identificación de particulares y la obtencion de los telefonos desde la web
 source("funciones/funciones.R")
 # Busqueda en nueva España
 # proccessDataFromIdealista("resultados","nuevaEsp",40.462926, -3.670845,555,pag=TRUE,deb=TRUE)
